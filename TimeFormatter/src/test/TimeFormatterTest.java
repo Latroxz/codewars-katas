7cd7f6383659b03580c218bafc4268bb85cd7897
Latroxz <lalosantos06@gmail.com>
2025-04-04 20:32:12 -0600

    Fixed clrf to LF

--- TimeFormatter/src/test/TimeFormatterTest.java
+++ TimeFormatter/src/test/TimeFormatterTest.java
@@ -17,6 +17,6 @@ public class TimeFormatterTest {
         assertEquals("2 minutes", TimeFormatter.formatDuration(120));
         assertEquals("1 hour", TimeFormatter.formatDuration(3600));
         assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
-
     }
+
 }
