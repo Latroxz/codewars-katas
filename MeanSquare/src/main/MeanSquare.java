453d9852948449d0d43111f81127931442ae474a
Latroxz <lalosantos06@gmail.com>
2025-04-15 08:34:11 -0600

    added tests for TDD

--- MeanSquare/src/main/MeanSquare.java
+++ MeanSquare/src/main/MeanSquare.java
@@ -1,4 +1,14 @@
 package main;
 
 public class MeanSquare {
+    public static double solution(int[] arrA, int[] arrB) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 5, 6};
+        System.out.println(solution(a1, a2));
+    }
+
 }
