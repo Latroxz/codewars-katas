57abaad2cbe1dc8865489543c436145521465c1c
Latroxz <lalosantos06@gmail.com>
2025-04-15 09:17:26 -0600

    Added streams to the meansquare method as a modern solution

--- MeanSquare/src/main/MeanSquare.java
+++ MeanSquare/src/main/MeanSquare.java
@@ -16,6 +16,8 @@ Examples
 
  */
 
+import java.util.stream.IntStream;
+
 public class MeanSquare {
     public static double solution(int[] arrA, int[] arrB) {
         double mean = 0d;
@@ -26,10 +28,19 @@ public class MeanSquare {
         return mean;
     }
 
+    public static double solutionStreams(int[] arrA, int[] arrB) {
+
+        return IntStream.range(0, arrA.length)
+                .mapToDouble(index -> Math.pow(arrA[index] - arrB[index], 2))
+                .average()
+                .getAsDouble();
+    }
+
     public static void main(String[] args) {
         int[] a1 = {1, 2, 3};
         int[] a2 = {4, 5, 6};
         System.out.println(solution(a1, a2));
+        System.out.println(solutionStreams(a1, a2));
     }
 
 }
--- MeanSquare/src/test/MeanSquareTest.java
+++ MeanSquare/src/test/MeanSquareTest.java
@@ -29,4 +29,26 @@ public class MeanSquareTest {
 
     }
 
+    @Test
+    @DisplayName("It should return the mean squared difference between 2 matrix using streams")
+    public void shouldReturnTheMeanDifferenceSquareUsingStreams() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 5, 6};
+        assertEquals(9, MeanSquare.solutionStreams(a1, a2));
+
+        int[] b1 = {10, 20, 10, 2};
+        int[] b2 = {10, 25, 5, -2};
+
+        assertEquals(16.5, MeanSquare.solutionStreams(b1, b2));
+
+        int[] c1 = {0, -1};
+        int[] c2 = {-1, 0};
+        assertEquals(1.0, MeanSquare.solutionStreams(c1, c2));
+
+        int[] d1 = {10, 10};
+        int[] d2 = {10, 10};
+        assertEquals(0.0, MeanSquare.solutionStreams(d1, d2));
+
+    }
+
 }
