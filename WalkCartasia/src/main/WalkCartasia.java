c6552e6411c7157f9f012c9d8ae82bdf43bf4695
Latroxz <lalosantos06@gmail.com>
2025-04-16 05:25:34 -0600

    Problem solved, all tests passing

--- WalkCartasia/src/main/WalkCartasia.java
+++ WalkCartasia/src/main/WalkCartasia.java
@@ -2,7 +2,21 @@ package main;
 
 public class WalkCartasia {
     public static boolean isValid(char[] coordinates) {
-        return true;
+        if (coordinates.length != 10) return false;
+        int x = 0;
+        int y = 0;
+        for (char coordinate : coordinates) {
+            switch (coordinate) {
+                case 'n' -> y++;
+                case 's' -> y--;
+                case 'e' -> x++;
+                case 'w' -> x--;
+                default -> {
+                    return false;
+                }
+            }
+        }
+        return x == 0 && y == 0;
     }
 
     public static void main(String[] args) {
