220b63aec75e88f36beb7d87f7108ad33d888801
Latroxz <lalosantos06@gmail.com>
2025-04-16 04:13:40 -0600

    Problem solved, added clean code approach, all tests passing

--- IntruderNumber/src/main/IntruderNumber.java
+++ IntruderNumber/src/main/IntruderNumber.java
@@ -2,7 +2,44 @@ package main;
 
 public class IntruderNumber {
     public static int find(int[] numbers) {
+        Integer firstPositions = intruderInFirstPositions(numbers);
+        if (firstPositions != null) return firstPositions;
+        return intruderInRemainingPositions(numbers);
+    }
+
+    private static int intruderInRemainingPositions(int[] numbers) {
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+            if (evenCount > 1) {
+                if (number % 2 != 0) return number;
+            } else if (oddCount > 1) {
+                if (number % 2 == 0) return number;
+            }
+        }
         return 0;
+    }
+
+    private static Integer intruderInFirstPositions(int[] numbers) {
+        if (numbers.length > 2) {
+            if ((numbers[0] % 2 == numbers[2] % 2) && numbers[1] % 2 != numbers[0] % 2) {
+                return numbers[1];
+            }
+            if ((numbers[1] % 2 == numbers[2] % 2) && numbers[0] % 2 != numbers[1] % 2) {
+                return numbers[0];
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781};
+        System.out.println(find(numbers));
     }
 
 }
--- IntruderNumber/src/test/IntruderNumberTest.java
+++ IntruderNumber/src/test/IntruderNumberTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntruderNumberTest {
     @Test
-    public void sampleTests() {
+    public void shouldReturnTheIntruderInEvenOrOddArray() {
         assertEquals(3, IntruderNumber.find(new int[]{2, 6, 8, -10, 3}));
         assertEquals(206847684, IntruderNumber.find(new int[]{206847684, 1056521, 7, 17, 1901, 21104421, 7, 1,
                 35521, 1, 7781}));
