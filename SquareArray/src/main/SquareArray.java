552fa6348759f1859f11e2805fe89fcf403ed993
Latroxz <lalosantos06@gmail.com>
2025-04-16 07:32:05 -0600

    Problem solved, all tests are correct

--- SquareArray/src/main/SquareArray.java
+++ SquareArray/src/main/SquareArray.java
@@ -1,13 +1,37 @@
 package main;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class SquareArray {
 
     public static boolean comp(int[] a, int[] b) {
-        return false;
+        if (Objects.isNull(a) || Objects.isNull(b) || (a.length != b.length)) return false;
+        int[] numbersSqrt = getElementsSqrt(b);
+        a = getArrayAbsoluteAndSorted(a);
+        Arrays.sort(numbersSqrt);
+
+        return Arrays.equals(a, b);
+    }
+
+    private static int[] getArrayAbsoluteAndSorted(int[] a) {
+        return Arrays.stream(a)
+                .map(Math::abs)
+                .sorted()
+                .toArray();
+    }
+
+    private static int[] getElementsSqrt(int[] b) {
+        for (int i = 0; i < b.length; i++) {
+            b[i] = (int) Math.sqrt(b[i]);
+        }
+        return b;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        System.out.println("Are they Equal: " + comp(a, b));
     }
 
 }
--- SquareArray/src/test/SquareArrayTest.java
+++ SquareArray/src/test/SquareArrayTest.java
@@ -7,16 +7,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareArrayTest {
+    int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+    int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
+    int[] c = new int[]{131, 144, 19, 161, 19, 144, 19, 11};
 
     @Test
     @DisplayName("Should return true if the second array is the square of the first")
     public void shouldReturnTrueIfSecondArrayIsSquaredOfFirst() {
-        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
-        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
-        int[] c = new int[]{131, 144, 19, 161, 19, 144, 19, 11};
 
         assertEquals(true, SquareArray.comp(a, b));
+    }
+
+    @Test
+    @DisplayName("Should return false if the second array isnt the square of the first")
+    public void shouldReturnFalseIfSecondArrayIsNotSquaredOfFirst() {
         assertEquals(false, SquareArray.comp(a, c));
+    }
+
+    @Test
+    @DisplayName("Should return false if the second array isnt the same length of the first")
+    public void shouldReturnFalseIfSecondArrayIsNotTheSameLength() {
+        assertEquals(false, SquareArray.comp(a, new int[]{1, 2, 3}));
+        assertEquals(false, SquareArray.comp(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3}));
+
+    }
+
+    @Test
+    @DisplayName("Should return false if the second array isnt the same length of the first")
+    public void shouldReturnFalseIfArrayIsNull() {
+        assertEquals(false, SquareArray.comp(null, new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void shouldReturnTrueIfArraysAreEmpty() {
+        assertEquals(true, SquareArray.comp(new int[]{}, new int[]{}));
     }
 
 }
