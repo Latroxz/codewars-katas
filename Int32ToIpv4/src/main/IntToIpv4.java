e0cb1816688cf106012bb6a20e10c5dfb3003ea3
Latroxz <lalosantos06@gmail.com>
2025-04-16 01:46:34 -0600

    Finished exercise, used clean code to better legibility, all tests passing

--- Int32ToIpv4/src/main/IntToIpv4.java
+++ Int32ToIpv4/src/main/IntToIpv4.java
@@ -3,11 +3,31 @@ package main;
 public class IntToIpv4 {
 
     public static String longToIP(long ip) {
-        return "";
+        StringBuilder ipBinary = getBinaryIp(ip);
+        StringBuilder ipv4 = getIpv4(ipBinary);
+        return ipv4.toString();
+    }
+
+    private static StringBuilder getIpv4(StringBuilder ipBinary) {
+        StringBuilder ipv4 = new StringBuilder();
+        while (!ipBinary.isEmpty()) {
+            ipv4.append(Integer.parseInt(ipBinary.substring(0, 8), 2));
+            ipv4.append(".");
+            ipBinary.delete(0, 8);
+        }
+        // delete last . in result xx.xx.xx.xx.
+        ipv4.setLength(ipv4.length() - 1);
+        return ipv4;
+    }
+
+    private static StringBuilder getBinaryIp(long ip) {
+        return new StringBuilder(String.format("%32s", Long.toBinaryString(ip)).replace(" ", "0"));
     }
 
     public static void main(String[] args) {
-
+        longToIP(32L); // 0.0.0.32
+        longToIP(2147483648L); // 128.0.0.0
+        longToIP(2149583361L);
     }
 
 }
--- Int32ToIpv4/src/test/IntToIpv4Test.java
+++ Int32ToIpv4/src/test/IntToIpv4Test.java
@@ -8,10 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntToIpv4Test {
     @Test
-    @DisplayName("It should convert a given int32 ip to the value in octets")
+    @DisplayName("It should convert a given int32 IP to the value in Ipv4")
     public void shouldConvertIntIpToOctets() {
         assertEquals("128.114.17.104", IntToIpv4.longToIP(2154959208L));
         assertEquals("0.0.0.0", IntToIpv4.longToIP(0));
+        assertEquals("0.0.0.32", IntToIpv4.longToIP(32L));
         assertEquals("128.32.10.1", IntToIpv4.longToIP(2149583361L));
     }
 
