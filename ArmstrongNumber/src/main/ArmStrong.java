ba13dc7c61da89484b6c333b5ad644c5946b4d2e
Latroxz <lalosantos06@gmail.com>
2025-04-16 22:07:58 -0600

    Added tests for TDD, added readme for description

---
+++ ArmstrongNumber/src/main/ArmStrong.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ArmStrong {
+    public static boolean isNarcissistic(int number) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is it narcissistic? " + isNarcissistic(153));
+    }
+
+}
