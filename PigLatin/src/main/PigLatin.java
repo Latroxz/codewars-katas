5edeb05918a920542fdf9b74c87fe2cd3e32de19
Latroxz <lalosantos06@gmail.com>
2025-04-16 23:48:20 -0600

    Added readme and unit tests for TDD

---
+++ PigLatin/src/main/PigLatin.java
@@ -0,0 +1,12 @@
+package main;
+
+public class PigLatin {
+    public static String pigIt(String str) {
+        // Write code here
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
