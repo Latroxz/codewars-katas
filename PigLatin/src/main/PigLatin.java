2d93e0621a8eae0e99fb09a21653b10f65ebcd3f
Latroxz <lalosantos06@gmail.com>
2025-04-17 00:27:33 -0600

    Problem solved using streams, all tests passing

--- PigLatin/src/main/PigLatin.java
+++ PigLatin/src/main/PigLatin.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class PigLatin {
-    public static String pigIt(String str) {
-        // Write code here
+    public static String pigIt(String sentence) {
+        return Arrays.stream(sentence.trim()
+                        .split(" "))
+                .map(word -> word.matches("\\p{Punct}") ? word : word.substring(1) + word.charAt(0) + "ay")
+                .collect(Collectors.joining(" "));
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(pigIt("Pig latin is cool"));
     }
 
 }
--- PigLatin/src/test/PigLatinTest.java
+++ PigLatin/src/test/PigLatinTest.java
@@ -10,6 +10,8 @@ public class PigLatinTest {
     public void FixedTests() {
         assertEquals("igPay atinlay siay oolcay", PigLatin.pigIt("Pig latin is cool"));
         assertEquals("hisTay siay ymay tringsay", PigLatin.pigIt("This is my string"));
+        assertEquals("elloHay orldway !", PigLatin.pigIt("Hello world !"));
+
     }
 
 }
