8d0eb3c59ec6e1a3c975c28c39c75c671f9128d2
Latroxz <lalosantos06@gmail.com>
2025-04-18 06:04:27 -0600

    Problem solved using hashmap, all tests passing

--- CountingDuplicates/src/main/CountingDuplicates.java
+++ CountingDuplicates/src/main/CountingDuplicates.java
@@ -1,12 +1,23 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountingDuplicates {
     public static int duplicateCount(String word) {
-        return 0;
+        Map<Character, Integer> duplicated = new HashMap<>();
+        for (char c : word.toLowerCase()
+                .toCharArray()) {
+            duplicated.put(c, duplicated.getOrDefault(c, 0) + 1);
+        }
+        return (int) (duplicated.values()
+                .stream()
+                .filter(count -> count > 1)
+                .count());
     }
 
     public static void main(String[] args) {
-        System.out.println("duplicate count: " + duplicateCount("abcdea"));
+        System.out.println("duplicate count: " + duplicateCount("Indivisibilitiesrr"));
     }
 
 }
