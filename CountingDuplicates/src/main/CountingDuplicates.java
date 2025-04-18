77e2fc22492217d32da9d18731dc74e52bb90db8
Latroxz <lalosantos06@gmail.com>
2025-04-18 05:29:06 -0600

    Added unit tests for TDD, added readme

---
+++ CountingDuplicates/src/main/CountingDuplicates.java
@@ -0,0 +1,12 @@
+package main;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String word) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("duplicate count: " + duplicateCount("abcdea"));
+    }
+
+}
