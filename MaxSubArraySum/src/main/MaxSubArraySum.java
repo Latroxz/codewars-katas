2d66af1b96c794a002646c4ff4ddb1ac376215f9
Latroxz <lalosantos06@gmail.com>
2025-04-21 13:55:24 -0600

    Added tests for TDD, added readme for problem

---
+++ MaxSubArraySum/src/main/MaxSubArraySum.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MaxSubArraySum {
+
+    public static int sequence(int[] numbers) {
+
+        return 0;
+    }
+
+}
---
+++ MaxSubArraySum/src/test/MaxSubArraySumTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import main.MaxSubArraySum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaxSubArraySumTest {
+    @Test
+    @DisplayName("Empty arrays should have a max of 0")
+    public void shouldReturn0WhenArrayIsEmpty() throws Exception {
+        assertEquals(0, MaxSubArraySum.sequence(new int[]{}));
+    }
+
+    @Test
+    @DisplayName("Example array should have a max of 6")
+    public void shouldReturnMaxSubArraySum() throws Exception {
+        assertEquals(6, MaxSubArraySum.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+}
